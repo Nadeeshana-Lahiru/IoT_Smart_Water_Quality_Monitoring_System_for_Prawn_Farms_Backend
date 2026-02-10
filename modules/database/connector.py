af7a3c8d760682bd8442961773d3a95f26199b2b
Charitha Prabhashwara <prabhashwara.seu@gmail.com>
2026-02-10 20:51:39 +0530

    update connector

--- modules/database/connector.py
+++ modules/database/connector.py
@@ -1,14 +1,13 @@
 import sqlite3
 import os
 
-
 APP_ROOT = os.getcwd()
-DB_PATH = os.path.join(APP_ROOT, "database/data.db")
+DB_DIR = os.path.join(APP_ROOT, "database")
+DB_PATH = os.path.join(DB_DIR, "data.db")
 
 def connect():
-   
-    connection = sqlite3.connect(DB_PATH,check_same_thread=False)
+    os.makedirs(DB_DIR, exist_ok=True)
+
+    connection = sqlite3.connect(DB_PATH, check_same_thread=False)
     connection.row_factory = sqlite3.Row
     return connection
-
-
